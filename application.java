564094dd2e27786a2bfac71974f87d481d6f9890
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 14:48:35 +0530

    application.java

--- application.java
+++ application.java
@@ -16,3 +16,23 @@ public class EvenOdd {
         }
     }
 }
+# code changed by featur202
+public class PrimeCheck {
+    public static void main(String[] args) {
+        int num = 13;
+        boolean isPrime = true;
+
+        for (int i = 2; i <= num / 2; i++) {
+            if (num % i == 0) {
+                isPrime = false;
+                break;
+            }
+        }
+
+        if (isPrime)
+            System.out.println(num + " is a Prime Number");
+        else
+            System.out.println(num + " is Not a Prime Number");
+    }
+}
+# end 
