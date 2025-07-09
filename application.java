f5a663347df9ea08a6dafb56ebab909fa48e389b
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 14:23:29 +0530

    appication.java

--- application.java
+++ application.java
@@ -5,3 +5,14 @@ public class AddNumbers {
         System.out.println("Sum = " + sum);
     }
 }
+# code changed by feature201
+public class EvenOdd {
+    public static void main(String[] args) {
+        int num = 7;
+        if (num % 2 == 0) {
+            System.out.println(num + " is Even");
+        } else {
+            System.out.println(num + " is Odd");
+        }
+    }
+}
