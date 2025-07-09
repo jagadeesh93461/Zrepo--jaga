8a38b21b4189fc0eabd3c61d2c5deb3f506f7f35
jagadeesh93461 <konetijagadeesh7@gmail.com.com>
2025-07-09 14:30:04 +0530

    appication.java

--- application.java
+++ application.java
@@ -5,3 +5,14 @@ public class AddNumbers {
         System.out.println("Sum = " + sum);
     }
 }
+# code changed 202
+public class Factorial {
+    public static void main(String[] args) {
+        int num = 5;
+        int fact = 1;
+        for (int i = 1; i <= num; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial of " + num + " is " + fact);
+    }
+}
